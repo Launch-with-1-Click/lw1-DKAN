f3f7d7a627f5cbfff8b81a994989a379c634dae9
sawanoboly <sawanoboriyu@higanworks.com>
2017-06-15 18:05:39 +0900

    update DKAN to 7.x-1.13.3

--- site-cookbooks/lw1_dkan/attributes/defaults.rb
+++ site-cookbooks/lw1_dkan/attributes/defaults.rb
@@ -1,8 +1,8 @@
 ## Sum = Sha256
-# https://github.com/NuCivic/dkan/archive/7.x-1.12.5.tar.gz
+# https://github.com/NuCivic/dkan/archive/7.x-1.13.3.tar.gz
 default[:lw1_dkan][:install] = {
-  version: '7.x-1.12.5',
-  checksum: "ca7624b2fccc325c8ad5f76a157889c5ab49d6742bfeb9da7d9287a4c81c90d0",
+  version: '7.x-1.13.3',
+  checksum: "0e662210eca75aa0a9945a8a9a082f85b13f9f7622b1ed7c936432c2aeb52b79",
   base_url: "https://github.com/NuCivic/dkan/archive/"
 }
 
